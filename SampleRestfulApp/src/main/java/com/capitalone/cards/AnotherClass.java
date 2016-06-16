c0fb468781ecd3ec3ab0f3af1ced35242d3425bb
ChandraV2016 <moulicv@gmail.com>
2016-06-16 12:18:05 -0700

    Added another java file

---
+++ SampleRestfulApp/src/main/java/com/capitalone/cards/AnotherClass.java
@@ -0,0 +1,12 @@
+package com.capitalone.cards;
+
+public class AnotherClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World .....");
+		System.out.println("Added this line in GitHub .....");
+		
+	}
+
+}
